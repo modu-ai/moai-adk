129e25ca213f7813a42398dcddf126a9fec3c71f
Goos Kim <email@goos.kim>
2026-02-06 05:52:19 +0900

    chore: Bump version to v2.0.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 // Build-time variables injected via -ldflags.
 // Default version for RC/test builds (overridden by -ldflags in production)
 var (
-	Version = "v2.0.0-rc1"
+	Version = "v2.0.0"
 	Commit  = "none"
 	Date    = "unknown"
 )
