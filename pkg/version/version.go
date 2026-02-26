c5c888b80f48b25509a2cae005d85f4013af73ba
Goos Kim <email@goos.kim>
2026-02-26 22:41:34 +0900

    chore: bump version to v2.6.8

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ import "fmt"
 // Build-time variables injected via -ldflags.
 // Default version for RC/test builds (overridden by -ldflags in production)
 var (
-	Version = "v2.6.5"
+	Version = "v2.6.8"
 	Commit  = "none"
 	Date    = "unknown"
 )
